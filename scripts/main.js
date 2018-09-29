3bb127df318c804dc8c27703c9712f67a005a1cd
Tom <tom@localhost.localdomain>
2018-09-29 11:43:32 -0400

    Removed old code; going in a different direction

--- scripts/main.js
+++ scripts/main.js
@@ -3,23 +3,3 @@
  * @desc Main script file for the index page
 */
 
-var inField;
-var outField;
-var terminal;
-
-$(document).ready(function(){
-  inField = $('#inField');
-  outField = $('#outField');
-  terminal = $('#terminal');
-
-  terminal.on('submit', function(e){
-    var oldText = outField.val();
-    outField.val(oldText + inField.val() + '\n');
-    outField.scrollTop(outField[0].scrollHeight);
-
-    inField.val("");
-    e.preventDefault();
-  });
-
-
-});
